77e4afd0b672b2d1865898d24566d7e874d30f66
Ricardo Souza <ricardo.s.lima18@senai.br>
2026-01-23 21:41:00 -0300

    updates

--- LP/input_output.js
+++
@@ -1,5 +0,0 @@
-let i = 1; 
-while (i <= 20){ 
-    i += 2 
-    console.log(i);
-}
---
+++ input_output.js
@@ -0,0 +1,11 @@
+let senha = "pao";
+let senhadigitada = "";
+
+while (senhadigitada !== senha);{
+    senhadigitada = prompt("Digite a senha");
+    if (senhadigitada !== senha) {
+        alert("Senha incorreta. Try again loser")
+    }
+}
+
+alert("Senha correta. Wp");
