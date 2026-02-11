ea8ae4342c3f1ffdcc3dd14e533a7bcc135afc46
Ricardo Souza <ricardo.s.lima18@senai.br>
2026-02-10 21:32:57 -0300

    Project day one

---
+++ desenvolvimento-web/minha-spa/src/pages/Home.jsx
@@ -0,0 +1,8 @@
+export function Home(){
+    return (
+        <div>
+            <h1>Bem vindo à casa del carajo AKA SPA.</h1>
+            <p>This is an example of render via components.</p>
+        </div>
+    )
+}
