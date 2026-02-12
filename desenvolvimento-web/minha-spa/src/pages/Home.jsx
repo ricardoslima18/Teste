d39f38a017c99f86e64047088c2cda7d1c0c64d3
Ricardo Souza <ricardo.s.lima18@senai.br>
2026-02-11 21:39:10 -0300

    ahahhaha

--- desenvolvimento-web/minha-spa/src/pages/Home.jsx
+++ desenvolvimento-web/minha-spa/src/pages/Home.jsx
@@ -1,8 +1,14 @@
+import styles from '../styles/Home.module.css'
 export function Home(){
     return (
-        <div>
+        <div className={styles.container}>
+            <div>
             <h1>Bem vindo à casa del carajo AKA SPA.</h1>
             <p>This is an example of render via components.</p>
         </div>
-    )
+        <div className={styles.content}>
+            <p>bla bla bla bla</p>
+            </div>
+            </div>
+    );
 }
---
+++ desenvolvimento-web/minha-spa/src/pages/Sobre.jsx
@@ -0,0 +1,28 @@
+import styles from '../styles/Sobre.module.css' ;
+
+export function Sobre(){
+    return(
+        <div className={styles.container}>
+            <header classname={styles.header}>
+                <h1 classname={styles.title}> Sobre o projeto</h1>
+                <p className={styles.subtitle}>Conheça as tecnologias aplicadas nessa SPA.</p>
+            </header>
+
+        <section className={styles.content}>
+        <p>Este projeto bla bla bla bla<strong>React</strong></p>
+        <div className={styles.card}>
+            <h3>Capacidades Técnicas:</h3>
+            <ul className={styles.list}>
+            <li>Estrutura de Frame</li>
+            <li>Gerenciamento de Rotas</li>
+            <li>Estilização por componentes</li>
+            </ul>
+        </div>
+        </section>
+        <footer className={styles.footer}>
+        <p>Dsenvolvido durante o aprendizado de Front-End</p>
+        </footer>
+        </div>
+
+    );
+}
