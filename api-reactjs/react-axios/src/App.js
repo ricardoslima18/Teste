2366beedab07b9daa9c70856b9fd9f14e3c8176f
Ricardo Souza <ricardo.s.lima18@senai.br>
2026-02-20 21:31:48 -0300

    update aula dia 20/02

--- api-reactjs/react-axios/src/App.js
+++ api-reactjs/react-axios/src/App.js
@@ -1,25 +1,27 @@
-import logo from './logo.svg';
-import './App.css';
+import React, {useEffect, useState} from "react";
+import api from "./services/api";
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+export default function App() {
+    const [user, setUser] = useState(null);
+    
+    useEffect (() =>{
+      async function loadUser() {
+        try{
+          const response = await api.get('/use');
+          setUser(response.data);
+      } catch(error){
+        console.error('Deu erro babaca', error)
+      }
+    }
+    loadUser();
+    },[]);
+
+    if(!user) return <div>Carregando...</div>;
+    return(
+      <div className={App}>
+        <p>Usuário: {user.login}</p>
+        <p>Biografia: {user.bio || 'Sem biografia'}</p>
+      </div>
+    );
+
 }
-
-export default App;
