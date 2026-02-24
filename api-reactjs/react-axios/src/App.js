bdcc868ed720f4d5778601493d8e113f61529fdc
Ricardo Souza <ricardo.s.lima18@senai.br>
2026-02-23 21:27:50 -0300

    update 23/02

--- api-reactjs/react-axios/src/App.js
+++ api-reactjs/react-axios/src/App.js
@@ -1,27 +1,59 @@
-import React, {useEffect, useState} from "react";
+import {useEffect, useState} from "react";
 import api from "./services/api";
+import './App.css';
 
 export default function App() {
+    const [username, setUsername] = useState('ricardoslima18');
     const [user, setUser] = useState(null);
-    
+    const [repos, setRepos] = useState([]);
+    const [loading, setLoading] = useState(true);
+    const [error, setError] = useState(null);
+
+  // Carrega dados do api
     useEffect (() =>{
-      async function loadUser() {
-        try{
-          const response = await api.get('/users/ricardoslima18');
-          setUser(response.data);
+    async function loadData( userToSearch = username){
+      try {
+        setLoading(true);
+        setError(null);
+
+          //Carrega Usuario
+        const userResponse = await api.get(`/users/${userToSearch}`)
+        setUser(userResponse.data);
+
+          const reposResponse = await api.get(`/users/${userToSearch}/repos?per_page=6&sort=updated`)
+        setRepos(reposResponse.data);
+
       } catch(error){
-        console.error('Deu erro babaca', error)
+        console.error('Erro', error);
+        setError(`Usuário ${userToSearch} não encontrado`);
+      } finally {
+        setLoading(false);
       }
-    }
-    loadUser();
+      loadData();
+    };
     },[]);
 
-    if(!user) return <div>Carregando...</div>;
-    return(
-      <div className={App}>
-        <p>Usuário: {user.login}</p>
-        <p>Biografia: {user.bio || 'Sem biografia'}</p>
-      </div>
-    );
+    if(loading) {
+    return (
+  <div className="loading-container">
+  <div className="spinner"></div>
+  <p>Carregando perfil...</p>
+  </div>
 
-}
+    );
+  }
+   if (error || !user) {
+    return(
+      <div className="error-container">
+        <h2>❌ {error}</h2>
+        <button onClick={() => window.location.reload()}>Try again</button>    
+      </div>
+    )}
+    
+    return(
+   <div className="app-container">
+    <div className="profile-card"></div>
+
+    </div>
+      )}
+
